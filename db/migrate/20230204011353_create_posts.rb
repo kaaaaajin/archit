6537549449cefc6ae0d59db8d49958dfb8ae9b98
EC2 Default User <ec2-user@ip-172-31-34-226.ap-northeast-1.compute.internal>
2023-02-04 04:14:59 +0000

    devise

---
+++ db/migrate/20230204011353_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id, null: false
+      t.string :architecture, null: false
+      t.string :architect, null: false
+      t.string :address, null: false
+      t.float :latitude
+      t.float :longitude
+      t.string :country_code, null: false
+      t.text :body, null: false
+      
+      t.timestamps
+    end
+  end
+end
