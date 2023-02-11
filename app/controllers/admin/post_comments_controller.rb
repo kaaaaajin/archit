afc824590f576541d4ab046c044a513667eab2d1
EC2 Default User <ec2-user@ip-172-31-34-226.ap-northeast-1.compute.internal>
2023-02-11 13:35:52 +0000

    [add]admin

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,5 +1,10 @@
 class Admin::PostCommentsController < ApplicationController
+    before_action :authenticate_admin!
     
     def destroy
+        @post = Post.find(params[:post_id])
+        @post_comment = PostComment.find(params[:id])
+        @post_comment.destroy
     end
+
 end
