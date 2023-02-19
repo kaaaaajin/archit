402fc38dbced5052699c8cd056bbb0b576c1da3f
EC2 Default User <ec2-user@ip-172-31-34-226.ap-northeast-1.compute.internal>
2023-02-19 04:08:33 +0000

    pagenate

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,7 +8,7 @@ class Admin::UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
-        @posts = @user.posts
+        @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(9)
     end
     
     def edit
